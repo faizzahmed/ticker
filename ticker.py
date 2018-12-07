04aa116ec08ef04922cd6b9e41aac8a41281f7c7
Faiz Ahmed <faizahmed1@gmail.com>
2018-12-07 17:52:22 +0800

    ticker script init

---
+++ ticker.py
@@ -0,0 +1,10 @@
+# # Stock ticker
+import beautifulsoup 
+import requests
+
+endpoint = https://www.nseindia.com/live_market/dynaContent/live_watch/get_quote/GetQuote.jsp?symbol=
+symbol = TATAMOTORS
+
+url1 =  endpoint + symbol
+
+print (endpoint + symbol )
