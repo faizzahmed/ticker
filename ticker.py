31898b1df50777c0b51448a2f82fabc4e8c5d14c
Faiz Ahmed <faizahmed1@gmail.com>
2018-12-19 15:23:55 +0800

    error handling

--- ticker.py
+++ ticker.py
@@ -2,43 +2,66 @@
 from bs4 import BeautifulSoup
 import requests
 import json
-# import selenium  
+# import selenium
 
 # TODO - clean code someday
 
-# read file 
+# read file
 
-# GET NSE page 
+# GET NSE page
+
+
 def callNSE(symbol):
     pathVar = "https://www.nseindia.com/live_market/dynaContent/live_watch/get_quote/GetQuote.jsp?symbol="
-    finalURL =  pathVar + symbol
+    finalURL = pathVar + symbol
     page = requests.get(finalURL)
-    return page;
+    return page
 
-# # abstract Scraping logic here 
+# # abstract Scraping logic here
+
+
 def scrapeData(ReqSource):
     soup = BeautifulSoup(ReqSource.content, 'html.parser')
     divStruct = soup.find("div", {"id": "responseDiv"})
     divStructStr = str(divStruct)
-    jsonStruct = divStructStr.split('<div id="responseDiv" style="display:none">', 2)
+    jsonStruct = divStructStr.split(
+        '<div id="responseDiv" style="display:none">', 2)
     jsonStruct1 = str(jsonStruct[1])
-    jsonstruct2 = jsonStruct1.split('</div>',2)
+    jsonstruct2 = jsonStruct1.split('</div>', 2)
     finalJSON = str(jsonstruct2[0])
-    newDictionary=json.loads(str(finalJSON))['data']
-    datadict= newDictionary[0]
-    return datadict;
+    newDictionary = json.loads(str(finalJSON))['data']
+    if bool(newDictionary) == True:
+        respcode = 200
+        datadict = newDictionary[0]
+    else:
+        respcode = 404
+        datadict = ''
+    return datadict, respcode
 
 # __main__
-def main():   
-    stockListFile = open ( 'C:\\Projects\\ticker\\configStockNames.txt', 'r' )
-    for line in stockListFile: 
-        stock = line.rstrip('\n')
-        respSource = callNSE(stock)
-        stockData = scrapeData(respSource)
-        print('------------------------------------------------------------------------------')
-        print(stockData['symbol'] + '  ' + stockData['lastPrice'] +  '  ' + stockData['pChange']+'%'  + '  ' + stockData['change'] + ' High:' + stockData['dayHigh'] + ' Low:' + stockData['dayLow'])
 
-    print('------------------------------------------------------------------------------')
-     
+
+def main():
+    with open('C:\\Projects\\ticker\\configStockNames.txt') as stockListFile:
+        for line in stockListFile:
+            stock = line.rstrip('\n')
+            respSource = callNSE(stock)
+
+            stockData, nseresp = scrapeData(respSource)
+            print(
+                '------------------------------------------------------------------------------')
+
+            if nseresp == 200:
+                print(stockData['symbol'] + '  ' + stockData['lastPrice'] + '  ' + stockData['pChange']+'%' +
+                      '  ' + stockData['change'] + ' High:' + stockData['dayHigh'] + ' Low:' + stockData['dayLow'])
+            elif nseresp == 404:
+                print(stock + ' --> No data found')
+            else:
+                print('Call unsuccessful')
+
+        print(
+            '------------------------------------------------------------------------------')
+
+
 # invoke main
 main()
